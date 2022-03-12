435c4f3a126a7e584a13d05a212e154a08609969
Ankush Bahekar <ankushmb9@gmail.com>
2022-03-12 10:46:41 +0530

    [Ankush] Add: Ability to create a Contact in Address Book

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("!------Welcome to Address Book system------!");
+            //creating object of addressbook
+            AddressBook addressBook = new AddressBook();
+            //getting Op in Constructor with paratmers
+            addressBook.addContacts("Ankush", "Bahekar", "Mhalgi nagar", "Nagpur", "MH", 440022, 01234567890, "abcd@gmail.com");
+            // called Print method 
+            addressBook.print();
+            Console.ReadLine();
+        }
+    }
+}
